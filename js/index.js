32a7e9635fd69d36ea0511f3eee4207ca1be0a9c
Leandro Pelegrini <31962016+LeandroPelegrini@users.noreply.github.com>
2021-02-18 08:29:19 -0300

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){ 
-    
-  $('a.glitch').each(function(i){ 
-    var spanCont = $(this).html();
-    $(this).prepend('<span>'+spanCont+'</span>')
-           .append('<span>'+spanCont+'</span>');
-  }); 
-  
-});
